a8d53c7d631da45d47de9430fcff76c76954ee78
Sospeter Kungu <sospeter2030@gmail.com>
2020-05-01 21:29:42 +0300

    added js.cript files to the html and added add, subtract, divide and multipy functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,28 @@
 var add = function(number1, number2){
-return number1 + number2
-}
-
-var subtract = function(number1, number2) {
-return number1 - number2;
-};
-
-alert(subtract(11, 4));
-
-var add = function(number1, number2){
-	return number1 + number2
-};
-
-var number1 = prompt("Enter a number:");
-alert(number1);
-
-var ass = function(number1, number2){
-return number1 + number2
-};
-
-var number1 = prompt("Enter a number;");
-var number2 = prompt("Enter another number");
-alert(add(number1, number2));
-
-var add = function(number1, number2){
 	return number1 + number2;
 }
 var number1 = parseInt(prompt("Enter a number"));
 var number2 = parseInt(prompt("Enter another number"));
-alert(add(number1, number2));
+alert(add(number1, number2))
+
+var subtract = function(number1, number2){
+  return number1 - number2;
+}
+var number1 = parseInt(prompt("Enter a number:"))
+var number2 = parseInt(prompt("Enter another number:"))
+alert(subtract(number1, number2));
+
+var divide = function(number1, number2){
+  return number1 / number2;
+}
+var number1 = parseInt(prompt("Enter a number:"))
+var number2 = parseInt(prompt("Enter another number:"))
+alert(divide(number1, number2));
+
+var multiply = function(number1, number2){
+  return number1 * number2;
+}
+var number1 = parseInt(prompt("Enter a number:"))
+var number2 = parseInt(prompt("Enter another number"))
+var result = multiply(number1, number2)
+alert(result);
