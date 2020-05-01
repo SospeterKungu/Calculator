e7389ba7c8383533211fe6c8d8e0fed223a626ae
Sospeter Kungu <sospeter2030@gmail.com>
2020-05-01 17:01:18 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var add = function(number1, number2){
+return number1 + number2
+}
+
+var subtract = function(number1, number2) {
+return number1 - number2;
+};
+
+alert(subtract(11, 4));
+
+var add = function(number1, number2){
+	return number1 + number2
+};
+
+var number1 = prompt("Enter a number:");
+alert(number1);
+
+var ass = function(number1, number2){
+return number1 + number2
+};
+
+var number1 = prompt("Enter a number;");
+var number2 = prompt("Enter another number");
+alert(add(number1, number2));
+
+var add = function(number1, number2){
+	return number1 + number2;
+}
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter another number"));
+alert(add(number1, number2));
